e49880c3a475ab95ddff4c2dab8aa4074a4ec94a
Markus <mammutmk3@freenet.de>
2010-01-28 12:13:54 +0100

    something with the echo filter

--- src/main_markus.cpp
+++ src/main_markus.cpp
@@ -14,6 +14,7 @@ extern "C"{
 #include "types.h"
 #include "visual/writePPM.h"
 #include "cpu_effects/edgeDetection.h"
+#include "cpu_effects/echoEffect.h"
 
 #include <iostream>
 #include <cstdlib>
@@ -41,7 +42,7 @@ int main(int argc, char** argv){
 
 	try{
 		// create the decoder for a video file
-		VideoReader reader("videos/testvid.mpg");
+		VideoReader reader("videos/testvid2.mpg");
 		VideoInfo vidInf = reader.getVideoInfo();
 
 		// create the new OpenCL program to process that video
@@ -58,29 +59,55 @@ int main(int argc, char** argv){
 		kernel.setKernelArgument(1, memMan.getOutImage_dev());
 
                 EdgeDetection edge;
+		EchoEffect echEff;
+
+                RGBA* output_pic = (RGBA*)malloc( vidInf.width * vidInf.height * sizeof( RGBA) );
+                RGBA* inpic0 = (RGBA*)malloc( vidInf.width * vidInf.height * sizeof( RGBA) );
+                RGBA* inpic1 = (RGBA*)malloc( vidInf.width * vidInf.height * sizeof( RGBA) );
+                RGBA* inpic2 = (RGBA*)malloc( vidInf.width * vidInf.height * sizeof( RGBA) );
+                RGBA* inpic3 = (RGBA*)malloc( vidInf.width * vidInf.height * sizeof( RGBA) );
+                RGBA* inpic4 = (RGBA*)malloc( vidInf.width * vidInf.height * sizeof( RGBA) );
+                inpic0 = NULL;
+		inpic1 = NULL;
+		inpic2 = NULL;
+		inpic3 = NULL;
+		inpic4 = NULL;
 
 		char filename[100];
 		for(int i=0;i<50;i++){
 			// decode the frame
 			reader.decodeNextFrame();
-	
-                        RGBA* output_pic = (RGBA*)malloc( vidInf.width * vidInf.height * sizeof( RGBA) );
-                        edge.sobelOperator( memMan.getInputFrame(), output_pic, vidInf.width, vidInf.height );
+
+			inpic4 = inpic3;
+			inpic3 = inpic2;
+			inpic2 = inpic1;
+			inpic1 = inpic0;
+			inpic0 = memMan.getInputFrame();
+
+			if ( inpic4 == NULL ) {
+				continue;
+			}
+
+			echEff.renderEcho( inpic0, inpic1, inpic2, inpic3, inpic4, output_pic, vidInf.width, vidInf.height );
+
+				
+       //sobel                 RGBA* output_pic = (RGBA*)malloc( vidInf.width * vidInf.height * sizeof( RGBA) );
+         //sobel               edge.sobelOperator( memMan.getInputFrame(), output_pic, vidInf.width, vidInf.height );
 
 			// copy it to the device
-//			cl_event cToDev = memMan.updateInputFrame();
+		//	cl_event cToDev = memMan.updateInputFrame();
 
 			// start the kernel
-//			cl_event sKernel = kernel.start(clrFltr.getCommandQueue(),cToDev, vidInf.width, vidInf.height, 16, 16);
+		//	cl_event sKernel = kernel.start(clrFltr.getCommandQueue(),cToDev, vidInf.width, vidInf.height, 16, 16);
 
 			// copy the results back
-//			memMan.updateOutputFrame(sKernel);
+		//	memMan.updateOutputFrame(sKernel);
 
 			// write the image to output
 			sprintf(filename,"pictures/pic%i.ppm",i);
 
 //			writePPM(memMan.getOutputFrame(),filename,vidInf.width, vidInf.height);
-			writePPM( output_pic,filename,vidInf.width, vidInf.height);
+			writePPM( output_pic ,filename,vidInf.width, vidInf.height);
 		}
 		
 	
